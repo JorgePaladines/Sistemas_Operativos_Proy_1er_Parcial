1f562f1b5701d02e6837ab2244e0ab67a5f69091
Jorge Paladines <jpaladin@espol.edu.ec>
2019-06-21 15:08:41 -0500

    Cambié el orden de unas líneas de código y quité la declaración de variables que sólo se usan 1 vez. También metí la función imwrite dentro del if() para que sea más rápido.

--- concurrent_filter_demo_SO.cpp
+++ concurrent_filter_demo_SO.cpp
@@ -22,35 +22,44 @@ using namespace cv;
 /** @function main */
 int main ( int argc, char** argv )
 {
-  /// Declare variables
-  Mat src, dst, dstx, dsty;
-  Mat kernel;
-  Point anchor;
-  double delta;
-  int ddepth;
-  int kernel_size;
-
   /// Load an image
-  src = imread( argv[1] );
-
+  Mat src = imread( argv[1] );
   if( !src.data ){
     return -1;
   }
+  imshow( "imagen original", src );
+
+  /// Declare variables
+  Mat dst;
+  //Mat kernel;
+  //Point anchor;
+  //double delta;
+  //int ddepth;
   
   /// Initialize arguments for the filter
-  anchor = Point( -1, -1 );
-  delta = 0;
-  ddepth = -1;
-  imshow( "imagen original", src ); //remover
-
+  //anchor = Point( -1, -1 );
+  //delta = 0;
+  //ddepth = -1;
+  
   //Gaussian
-  kernel=getGaussianKernel(15,3);
-  filter2D(src, dst, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
-  bool isSuccess = imwrite("output_lena.jpg",dst); 
+  
+  /*
+  Quizá la función getGaussianKernel se la puede hacer concurrente, sin embargo igual
+  esta se nececita para la función filter2D.
+  Aunque se la haga en hilos, todos deben de terminar antes de llamar a filter2D
 
-  if (isSuccess == true) //remover
+  Así que se podría hacer concurrente la función getGaussianKernel para que termine más
+  rápido, y luego hacer concurrente filter2D.
+  */
+
+  //kernel=getGaussianKernel(15,3);
+  filter2D(src, dst, -1 , getGaussianKernel(15,3), Point( -1, -1 ), 0, BORDER_DEFAULT);
+  //bool isSuccess = imwrite("output_lena.jpg",dst); 
+
+  if (imwrite("output_lena.jpg",dst) == true) //remover
 	  imshow( "imagen filtrada", dst );
 
+  //Si se quita esta línea, hay un Segmentation Fault
   waitKey( 0 ); //remover
 
   return 0;
