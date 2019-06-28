1595f459122cf3408865bf535b53d2546d918c4d
Jorge Paladines <jpaladin@espol.edu.ec>
2019-06-27 20:20:36 -0500

    Traté de usar la función de parallel_for que viene dentro de opencv, pero no me sale bien. De todos modos metiéndole un Hola Mundo lo imprime la cantidad de veces que se especifica en el Range(0,N), así que se me hace que sólo usa Threads y ya

--- parallel_for_demo_SO.cpp
+++ parallel_for_demo_SO.cpp
@@ -5,15 +5,37 @@
 
 /*
 para compilar:
-g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 concurrent_filter_demo_SO.cpp -o ejemplo_filtro -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs
-para ejecutar:
-./ejemplo_filtro lena.jpg
-para medir el tiempo:
+g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 parallel_for_demo_SO.cpp -o parallel_ej -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs
 
-time ./ejemplo_filtro lena.jpg
+para ejecutar:
+./parallel_ej lena.jpg
+
+para medir el tiempo:
+time ./parallel_ej lena.jpg
+
 */
 
 using namespace cv;
+
+class Parallel_code: public cv::ParallelLoopBody
+{
+private:
+  Mat src,dst,kernel;
+
+public:
+  Parallel_code(Mat srcFile, Mat dstFile, Mat kernelOrg)
+    : src(srcFile), dst(dstFile), kernel(kernelOrg){}
+
+  void operator ()(const cv::Range &range) const{
+    for (int r = range.start; r < range.end; r++)
+    {
+      //filter2D(src, dst, -1 , kernel, Point( -1, -1 ), 0, BORDER_DEFAULT);
+    }
+    //Mat kernel = getGaussianKernel(15,3);
+    filter2D(src, dst, -1 , kernel, Point( -1, -1 ), 0, BORDER_DEFAULT);
+    //imwrite("output_lena.jpg",dst);
+  }
+};
 
 /** @function main */
 int main ( int argc, char** argv )
@@ -39,6 +61,9 @@ int main ( int argc, char** argv )
 
   //Gaussian
 
+  Mat kernel = getGaussianKernel(15,3);
+  parallel_for_(cv::Range(0,6), Parallel_code(src, dst,kernel));
+
   /*
   Quizá la función getGaussianKernel se la puede hacer concurrente, sin embargo igual
   esta se nececita para la función filter2D.
@@ -48,8 +73,8 @@ int main ( int argc, char** argv )
   */
 
   //kernel=getGaussianKernel(15,3);
-  filter2D(src, dst, -1 , getGaussianKernel(15,3), Point( -1, -1 ), 0, BORDER_DEFAULT);
-  bool isSuccess = imwrite("output_lena.jpg",dst);
+  //filter2D(src, dst, -1 , getGaussianKernel(15,3), Point( -1, -1 ), 0, BORDER_DEFAULT);
+  //bool isSuccess = imwrite("output_lena.jpg",dst);
 
   /*
   if (imwrite("output_lena.jpg",dst) == true) //remover
