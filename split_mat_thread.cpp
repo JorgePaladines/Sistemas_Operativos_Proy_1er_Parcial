0933fbd34b2373f9198399fd47ec436011476821
Jorge Paladines <jpaladin@espol.edu.ec>
2019-07-06 12:08:46 -0500

    split_mat_thread funciona con threads, un mutex, y joins

--- split_mat_thread.cpp
+++ split_mat_thread.cpp
@@ -2,8 +2,8 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <stdio.h>
-#include <iostream>
 #include <thread>
+#include <mutex>
 
 /*
 para compilar:
@@ -16,15 +16,21 @@ time ./split_mat_thread lena.jpg
 */
 
 using namespace cv;
+using namespace std;
 
-void fun_par(Mat sec[], Mat dst[]){
-  
+mutex image_mutex;
+
+void fun_par(vector<Mat> sec, vector<Mat> dst, Mat src, Mat kernel, int height, Mat3b res, int i){
+  sec[i] = src(Rect(0,height*i,src.cols,height));
+  filter2D(sec[i], dst[i], -1 , kernel, Point( -1, -1 ), 0, BORDER_DEFAULT);
+  image_mutex.lock();
+  dst[i].copyTo(res(Rect(0,dst[i].rows*i,src.cols,dst[i].rows)));
+  image_mutex.unlock();
 }
 
 /** @function main */
 int main ( int argc, char** argv )
 {
-  /// Load an image
   Mat src = imread( argv[1] );
   if( !src.data ){
     return -1;
@@ -32,18 +38,20 @@ int main ( int argc, char** argv )
 
   int cores = std::thread::hardware_concurrency(); //toma el número de cores del cpu
   int height = src.rows/cores;
-  Mat sec[cores];
-  Mat dst[cores];
+  vector<Mat> sec(cores);
+  vector<Mat> dst(cores);
 
   Mat kernel=getGaussianKernel(15,3);
 
   Mat3b res(src.rows, src.cols, Vec3b(0,0,0));
-  //Rect(x, y, w, h);
+
+  vector<thread> threads;
 
   for(int i = 0; i < cores; i++){
-    sec[i] = src(Rect(0,height*i,src.cols,height));
-    filter2D(sec[i], dst[i], -1 , kernel, Point( -1, -1 ), 0, BORDER_DEFAULT);
-    dst[i].copyTo(res(Rect(0,dst[i].rows*i,src.cols,dst[i].rows)));
+    threads.push_back(thread(fun_par,sec,dst,src,kernel,height,res,i));
+  }
+  for(int i = 0; i < cores; i++){
+    threads[i].join();
   }
 
   imwrite("output_lena.jpg",res);
